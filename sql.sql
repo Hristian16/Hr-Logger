58e5f0c60630542d6bca36a0bcc34f6f99d95c64
HristiyanRaykov <85462760+Hristian16@users.noreply.github.com>
2023-01-06 03:27:16 +0200

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,12 @@
+# Created by Hristian#8243
+
+CREATE TABLE `hr-logger` (
+  `name` longtext null,
+  `steam` longtext null,
+  `discord` longtext null,
+  `ip` longtext null,
+  `fivem` longtext null,
+  `xbl` longtext null
+);
+
+# Copyright Hristian#8243 © 2023
